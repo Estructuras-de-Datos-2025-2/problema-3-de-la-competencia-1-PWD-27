843a1391602ba0479a88f3f9b08855b98f456027
isc <pwd27@duck.com>
2025-08-29 11:48:07 -0400

    solucion

--- main.c
+++ main.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-int cmp_int(const void *a, const void *b)
-{ 
+static int cmp_int(const void *a, const void *b) {
     int x=*(const int*)a, y=*(const int*)b; 
     return (x>y)-(x<y); 
 }
+
 int main(void){
     int n;
-    scanf("%d",&n);
-    // TODO: malloc n, leer, qsort, compactar unicos, (opcional) realloc
-    // Salida:
-    // k
-    // a0 a1 ... a(k-1)
-    // TODO: free
+    if (scanf("%d",&n)!=1) 
+    return 0;
+
+    int *a = (int*)malloc((size_t)n*sizeof(int));
+    if(!a){ 
+    return 0;
+    }
+    for (int i = 0;i<n;++i){
+        if (scanf("%d",&a[i])!=1){
+            free(a);
+            return 0;
+        }
+    }
+    qsort(a,(size_t)n,sizeof(int),cmp_int);
+    int k=0;
+    for (int i = 0;i<n;++i){
+        if (k ==0 || a[i]!=a[k-1]){
+            a[k++]=a[i];
+        }
+    }
+
+    int*tmp=(int*)realloc(a,(size_t)k*sizeof(int));
+    if(tmp) a = tmp;
+
+    printf("%d\n", k);
+    for (int i = 0;i<k;++i){
+        if (i) putchar(' ');
+        printf("%d",a[i]);
+    }
+    putchar('\n');
+    free(a);
     return 0;
 }
